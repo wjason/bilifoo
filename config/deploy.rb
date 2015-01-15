9c489c3b738d6801cce549a4d4d63c721722953c
Jason Wu <cn.jason.wu@gmail.com>
2015-01-16 00:27:20 +0800

    fix the deploy asset

--- config/deploy.rb
+++ config/deploy.rb
@@ -27,7 +27,7 @@ set :linked_files, %w{config/database.yml}
 
 # Default value for linked_dirs is []
 # set :linked_dirs, fetch(:linked_dirs, []).push('bin', 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
-set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system public/uploads}
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
